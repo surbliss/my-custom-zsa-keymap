6bc899cc0b8abd96284eaa4edba8c9b824cbd75f
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-05-24 20:49:07 +0200

    Made CAPS_WORD work on - instead of +

--- JJypQ/keymap.c
+++ JJypQ/keymap.c
@@ -246,4 +246,26 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     'L', 'L', 'L', 'L', 'L', 'L',       'R', 'R', 'R', 'R', 'R', 'R',
     'L', 'L', 'L', 'L', 'L', 'L',       'R', 'R', 'R', 'R', 'R', 'R',
                         '*', '*',       '*', '*'
-  )
+);
+
+// Fix CAPS_WORD
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        // Danish placement of KC_MINS
+        case KC_SLHLS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
